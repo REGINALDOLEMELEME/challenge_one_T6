b3ee2894d48e68eeeaf996bfb239417041c9ed7a
Regis Leme <regis@gmail.com>
2024-01-29 14:06:57 -0300

    fix: ajustando a exibição dos textos traduzidos para display none / block do css

--- app.js
+++ app.js
@@ -9,21 +9,6 @@ function validarEntradaDoTexto(textarea) {
      textarea.value = entrada;
   }
 
-function criarBotaoCopiar(){
-
- // Cria um botão dinamicamente
- const botao = document.createElement('button');
- botao.textContent = 'Copiar';
-
- // Adiciona a classe 'minhaClasse' ao botão
- botao.classList.add('conteudo__texto__conversor__texto__botao');
- botao.onclick = copiarConteudo;
- 
-  // Adiciona o botão à seção
-  const secao = document.querySelector('.conteudo__texto_conversor');
-  secao.appendChild(botao);
-}
-
   function criptografarTexto() {
     // Obtém a referência da seção
     const secao = document.querySelector('.conteudo__texto_conversor');
@@ -35,16 +20,11 @@ function criarBotaoCopiar(){
     const textoAtual = document.getElementById('conteudo__texto__conversor__texto');
     const mensagemPrincipal = document.getElementById('conteudo__texto_conversor__mensagem');
     const imagem = document.querySelector('.conteudo__texto__conversor__imagem');
-    const botaoCopiar = document.querySelector('.conteudo__texto__conversor__texto__botao');
-    const mensagemNova = document.querySelector('.conteudo__texto_conversor__mensagem__nova');
+
     // Remove os elementos existentes
     if (textoAtual !== null) {
         secao.removeChild(textoAtual);
       }
-
-      if (mensagemNova !== null) {
-        secao.removeChild(mensagemNova);
-      }  
 
     if (mensagemPrincipal !== null) {
         secao.removeChild(mensagemPrincipal);
@@ -64,87 +44,60 @@ function criarBotaoCopiar(){
                  .replace(/o/g, 'ober')
                  .replace(/u/g, 'ufat');
   
-   
-    // Cria um novo parágrafo para a mensagem convertida
-    const mensagemConvertida = document.createElement('p');
+    mensagemConvertida = document.getElementById("conteudo__texto__conversor__mensagem__traduzida");
     mensagemConvertida.textContent = texto;
+    mensagemConvertida.style.display = "block";
+    document.getElementById("conteudo__texto__conversor__botao__copiar").style.display = "block";
 
-    if (botaoCopiar == null) {
-        criarBotaoCopiar();
-    }
-  
-  
-    // Adiciona a classe 'conteudo__texto_conversor__mensagem__nova' ao novo parágrafo
-    mensagemConvertida.classList.add('conteudo__texto_conversor__mensagem__nova');
-  
-    // Adiciona o botão e o novo parágrafo à seção
-    secao.appendChild(mensagemConvertida);
-   
   }
 
   function descriptografarTexto() {
-    // Obtém a referência da seção
-    const secao = document.querySelector('.conteudo__texto_conversor');
+       // Obtém a referência da seção
+       const secao = document.querySelector('.conteudo__texto_conversor');
   
-    // Obtém a referência do textarea
-    const textarea = document.getElementById('texto-para-converter');
-  
-    // Obtém a referência da tag p atual
-    const textoAtual = document.getElementById('conteudo__texto__conversor__texto');
-    const mensagemPrincipal = document.getElementById('conteudo__texto_conversor__mensagem');
-    const imagem = document.querySelector('.conteudo__texto__conversor__imagem');
-    const mensagemNova = document.querySelector('.conteudo__texto_conversor__mensagem__nova');
-    const botaoCopiar = document.querySelector('.conteudo__texto__conversor__texto__botao');
-
-    // Remove os elementos existentes
-    if (textoAtual !== null) {
-        secao.removeChild(textoAtual);
-      }
-        
-      if (mensagemNova !== null) {
-        secao.removeChild(mensagemNova);
-      }  
-
-    if (mensagemPrincipal !== null) {
-        secao.removeChild(mensagemPrincipal);
-    }
-
-    if (imagem !== null) {
-        secao.removeChild(imagem);
-    }
-  
-    // Obtém o texto do textarea
-    let texto = textarea.value;
-  
-    // Substitui as letras de acordo com a regra
-    texto = texto.replace(/enter/g, 'e')
-                 .replace(/imes/g, 'i')
-                 .replace(/ai/g, 'a')
-                 .replace(/ober/g, 'o')
-                 .replace(/ufat/g, 'u');
-  
-
-    // Cria um novo parágrafo para a mensagem convertida
-    const mensagemConvertida = document.createElement('p');
-    mensagemConvertida.textContent = texto;
-
-        
-    if (botaoCopiar == null) {
-        criarBotaoCopiar();
-     }
-
-  
-    // Adiciona a classe 'conteudo__texto_conversor__mensagem__nova' ao novo parágrafo
-    mensagemConvertida.classList.add('conteudo__texto_conversor__mensagem__nova');
-  
-    // Adiciona o botão e o novo parágrafo à seção
-    secao.appendChild(mensagemConvertida);
+       // Obtém a referência do textarea
+       const textarea = document.getElementById('texto-para-converter');
+     
+       // Obtém a referência da tag p atual
+       const textoAtual = document.getElementById('conteudo__texto__conversor__texto');
+       const mensagemPrincipal = document.getElementById('conteudo__texto_conversor__mensagem');
+       const imagem = document.querySelector('.conteudo__texto__conversor__imagem');
+   
+       // Remove os elementos existentes
+       if (textoAtual !== null) {
+           secao.removeChild(textoAtual);
+         }
+   
+       if (mensagemPrincipal !== null) {
+           secao.removeChild(mensagemPrincipal);
+       }
+   
+       if (imagem !== null) {
+           secao.removeChild(imagem);
+       }
+     
+       // Obtém o texto do textarea
+       let texto = textarea.value;
+     
+        // Substitui as letras de acordo com a regra
+        texto = texto.replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+     
+       mensagemConvertida = document.getElementById("conteudo__texto__conversor__mensagem__traduzida");
+       mensagemConvertida.textContent = texto;
+       mensagemConvertida.style.display = "block";
+       document.getElementById("conteudo__texto__conversor__botao__copiar").style.display = "block";
+   
      
   }
 
   function copiarConteudo() {
     // Obtém a referência do parágrafo com a mensagem nova
-    const mensagemNova = document.querySelector('.conteudo__texto_conversor__mensagem__nova');
+    const mensagemNova = document.getElementById('conteudo__texto__conversor__mensagem__traduzida');
+    const botaoCopiar = document.getElementById("conteudo__texto__conversor__botao__copiar")
   
     // Obtém o texto do parágrafo
     const texto = mensagemNova.textContent;
@@ -153,7 +106,7 @@ function criarBotaoCopiar(){
     navigator.clipboard.writeText(texto)
       .then(() => {
         // Exibe uma mensagem ou executa qualquer outra ação que você desejar
-        alert('Conteúdo copiado para a área de transferência!');
+        botaoCopiar.textContent = "Copiado";
       })
       .catch((err) => {
         console.error('Erro ao copiar para a área de transferência:', err);
